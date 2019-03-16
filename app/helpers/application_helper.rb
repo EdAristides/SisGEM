d0ab8c277e1a394827ee2531d28b3df6608a1e74
lmaoclost <renansmoliveira@gmail.com>
2019-03-16 20:13:45 -0300

    Layout responsivity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,30 @@ module ApplicationHelper
         end
         nil
     end
+
+    module BootstrapExtension
+    FORM_CONTROL_CLASS = "form-control"
+
+    # Override the 'text_field_tag' method defined in FormTagHelper[1]
+    #
+    # [1] https://github.com/rails/rails/blob/master/actionview/lib/action_view/helpers/form_tag_helper.rb
+    def text_field_tag(name, value = nil, options = {})
+      class_name = options[:class]
+      if class_name.nil?
+        # Add 'form-control' as the only class if no class was provided
+        options[:class] = FORM_CONTROL_CLASS
+      else
+        # Add ' form-control' to the class if it doesn't already exist
+        options[:class] << " #{FORM_CONTROL_CLASS}" if
+          " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+      end
+
+      # Call the original 'text_field_tag' method to do the real work
+      super
+    end
+  end
+
+  # Add the modified method to ApplicationHelper
+  include BootstrapExtension
+
 end
